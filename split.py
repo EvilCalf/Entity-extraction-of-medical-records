94ac033d797cadcdc88efe309cdca331c915b8be
EvilCalf <xujiading1997@163.com>
2021-01-06 09:11:19 +0800

    beautify codes

--- split.py
+++ split.py
@@ -1,3 +1,3 @@
-for index, line in enumerate(open('subtask1_training_part1.txt', 'r',encoding='UTF-8'), 1):
-    with open('yidu/%d.json' % index, 'w+',encoding='UTF-8') as tmp:
+for index, line in enumerate(open('subtask1_training_part1.txt', 'r', encoding='UTF-8'), 1):
+    with open('yidu/%d.json' % index, 'w+', encoding='UTF-8') as tmp:
         tmp.write(line)
